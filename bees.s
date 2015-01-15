d332dcfdac332c61948efb31377789d841484985
Max Bane <max.bane@gmail.com>
2015-01-15 01:00:36 -0500

    The basics of 2x2 actors alongside 1x1 actors; still need per-quadrant tiles and facing.

--- bees.s
+++ bees.s
@@ -57,8 +57,8 @@ main_palettes:
 
 ; Numeric constants
 .scope Constants
-    N_ACTORS                = 1
-    PLAYER_TILE_BASE        = 1
+    N_ACTORS                = 2
+    ;PLAYER_TILE_BASE        = 1
     ACTOR_BASE_OAM_SPRITENO = 1
     ; gravitational acceleration 
     GRAVITY_DDY             = $0004
@@ -70,23 +70,48 @@ main_palettes:
 
     FLOOR_Y                 = 231
     CEILING_Y               = 12
+    SCREEN_WIDTH            = 256
 .endscope
 
+.struct Vector
+    xval    .word
+    yval    .word
+.endstruct
+
+; How big an actor is
+.struct Actor
+    position    .tag Vector
+    velocity    .tag Vector
+    base_tile   .byte
+    flags       .byte
+.endstruct
+
+.enum ActorFlagMask
+    is_2x2  = %00000001
+.endenum
+
 .segment "ZEROPAGE"
-; Game variable "globals"
+; Reserve space for N_ACTORS actors, with a nice scope and labels for static
+; access to each. XXX Had better agree with the Actor struct above!!!
 .repeat Constants::N_ACTORS, I
     ;.out .sprintf ("actor_%02d", I)
     .scope .ident (.sprintf ("actor_%02d", I))
         actor_idx = I
         addr:
-        .scope velocity
-            xval:   .res 2
-            yval:   .res 2
-        .endscope
         .scope position
             xval:   .res 2
             yval:   .res 2
         .endscope
+        .scope velocity
+            xval:   .res 2
+            yval:   .res 2
+        .endscope
+        base_tile:  .res 1
+        flags:      .res 1
+        ; flags mask:
+        ; %76543210
+        ;         |
+        ;         +- is_2x2: Is actor 2x2 sprites (1) or 1 sprite (0)
     .endscope
 .endrepeat
 .define the_player actor_00
@@ -152,19 +177,7 @@ irq:
         bcc :-
 
     jsr PPU::clear_background
-
-    ; address a tile toward the middle of the screen
-    ;ldy #15
-    ;ldx #9
-    ;jsr PPU::address_tile
-    ; position player at x=64, y=64
-    ;mathmac_set16 #$4000, player_x_low, player_y_low
-    mathmac_set16 #$4000, the_player::position::xval, the_player::position::yval
-    ; zero velocity
-    mathmac_set16 #$0000, the_player::velocity::xval, the_player::velocity::yval
-
-    ; enable nmi-handler
-    ;jsr PPU::update
+    jsr init_actors
 
     ; loop forever
     @infiniloop:
@@ -177,13 +190,32 @@ irq:
     ; no rts
 .endproc
 
+; TODO : this should depend on level, etc.
+.proc init_actors
+    ; position player at x=64, y=64
+    mathmac_set16 #$4000, the_player::position::xval, the_player::position::yval, actor_01::position::yval
+    mathmac_set16 #$8000, actor_01::position::xval
+    ; zero velocity
+    mathmac_set16 #$0000, the_player::velocity::xval, the_player::velocity::yval
+    mathmac_set16 #$0000, actor_01::velocity::xval, actor_01::velocity::yval
+    lda #2
+    sta the_player::base_tile
+    lda #3
+    sta actor_01::base_tile
+    lda #(ActorFlagMask::is_2x2)
+    sta the_player::flags
+    lda #0
+    sta actor_01::flags
+    rts
+.endproc
+
 .proc handle_input
     ; buttons newly depressed this frame
     new_buttons = local_0
     jsr Joy::lda_new_buttons
     sta new_buttons
 
-    ; A button: jump
+    ; A button: flap
     and #Joy::BUTTON_A
     beq :+
         jsr do_flap
@@ -250,10 +282,28 @@ irq:
             lda py+1
             cmp #Constants::CEILING_Y
             bcc hit_ceiling
+            ; 2x2 actors effectively have their floor shifted up by 8 pixels
+            lda actor_i::flags
+            and #ActorFlagMask::is_2x2
+            beq :+
+                ; 2x2 actor
+                lda py+1
+                cmp #(Constants::FLOOR_Y - 8)
+                bcc collision_done
+                jmp hit_floor
+            :
+            ; 1x1 actor
+            lda py+1
             cmp #Constants::FLOOR_Y
             bcc collision_done
             hit_floor:
                 ; clip to floor
+                lda actor_i::flags
+                and #ActorFlagMask::is_2x2
+                beq :+
+                    mathmac_set16 #((Constants::FLOOR_Y - 9) * $100), py
+                    jmp hit_common
+                :
                 mathmac_set16 #((Constants::FLOOR_Y - 1) * $100), py
                 jmp hit_common
             hit_ceiling:
@@ -275,25 +325,111 @@ irq:
 .endproc
 
 .proc draw_actors
+    ; address of current 4-byte oam buffer entry we are updating
+    buffer_entry_addr = addr_0
+    lda #<PPU::oam_buffer
+    sta buffer_entry_addr
+    lda #>PPU::oam_buffer
+    sta buffer_entry_addr + 1
     .repeat Constants::N_ACTORS, I
         .define actor_i .ident (.sprintf ("actor_%02d", I))
         .scope 
-            oam_entry_idx = PPU::oam_buffer + \
-                (Constants::ACTOR_BASE_OAM_SPRITENO + I) * 4
+            ldy #0
             ; high bytes of actor's little endian 16-bit position coords become
             ; screen coords
+            ; Y screen coord
             lda actor_i::position::yval + 1
-            sta oam_entry_idx + 0
-            ; TODO: per-actor tiles and oam attributes
-            lda #Constants::PLAYER_TILE_BASE
-            sta oam_entry_idx + 1
+            sta (buffer_entry_addr), Y
+            iny
+            ; Tile number
+            lda actor_i::base_tile
+            sta (buffer_entry_addr), Y
+            iny
+            ; OAM flags
+            ; TODO compute OAM flags from actor state
             lda #0
-            sta oam_entry_idx + 2
+            sta (buffer_entry_addr), Y
+            iny
+            ; X screen coord
             lda actor_i::position::xval + 1
-            sta oam_entry_idx + 3
+            sta (buffer_entry_addr), Y
+            ; check actor flags
+            lda actor_i::flags
+            and #ActorFlagMask::is_2x2
+            beq :+
+                ldx #actor_i::addr
+                jsr fill_2x2_actor_remaining_sprites
+            :
+            .if I < Constants::N_ACTORS - 1
+                ; advance the buffer entry pointer
+                lda buffer_entry_addr ; 3 cycles
+                clc                   ; 2 cycles
+                adc #4                ; 2 cycles
+                sta buffer_entry_addr ; 3 cycles
+            .endif
         .endscope
         .undefine actor_i
     .endrepeat
     rts
 .endproc
 
+; Call with X = zp addr of actor, addr_0 = address of actor's first OAM buffer
+; entry
+.proc fill_2x2_actor_remaining_sprites
+    ; hm, ends up lookig a lot the same as draw_actors
+    ; Quadrants of a 2x2 sprite. The unlabled quadrant has already been drawn
+    ;   | 0  
+    ; --+--
+    ; 2 | 1
+    buffer_entry_ptr = addr_0
+    ; if position::xval is far to the right onscreen, skip quadrants 0 and 1
+    lda 1, X      ; A = MSB(actor::position::xval)
+    cmp #(Constants::SCREEN_WIDTH - 8)
+    bcc :+
+        ; Actor's x-coord is < 8 pixels from right edge. Hide quadrants 0 and 1
+        ; by setting their screen y to $FF, then jump to drawing quadrant 2
+        clc
+        ldy #0
+        lda buffer_entry_ptr
+        adc #4
+        sta buffer_entry_ptr
+        lda #$ff
+        sta (buffer_entry_ptr), Y
+        lda buffer_entry_ptr
+        adc #4
+        sta buffer_entry_ptr
+        lda #$ff
+        sta (buffer_entry_ptr), Y
+        jmp quadrant_2
+    :
+    .repeat 3, I
+        .ident (.sprintf ("quadrant_%d", I)):
+        .if I = 2
+            clc
+        .endif
+        ; advance to next buffer entry
+        lda buffer_entry_ptr 
+        adc #4                
+        sta buffer_entry_ptr 
+        ldy #0
+        lda 3, X      ; A = MSB(actor::position::yval)
+        .if I > 0
+            adc #8
+        .endif
+        sta (buffer_entry_ptr), Y
+        iny
+        lda 8, X      ; A = actor::base_tile
+        sta (buffer_entry_ptr), Y
+        iny
+        ; TODO compute OAM flags from actor state
+        lda #0
+        sta (buffer_entry_ptr), Y
+        iny
+        lda 1, X      ; A = MSB(actor::position::xval)
+        .if I < 2
+            adc #8
+        .endif
+        sta (buffer_entry_ptr), Y
+    .endrepeat
+    rts
+.endproc
